8a38a5943014db5c0158369d65c78ab8b9f5ee58
AkanshaRawat01 <88279484+AkanshaRawat01@users.noreply.github.com>
2022-09-03 09:33:54 +0530

    Initial commit

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Nikunja',
+    lastName: 'Joshi',
+    email: 'nikunja@gmail.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Prachi',
+    lastName: 'Pant',
+    email: 'prachi@gmail.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Anshika',
+    lastName: 'Takuli',
+    email: 'anshika@gmail.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Sejal',
+    lastName: 'Bisht',
+    email: 'sejal@gmail.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Monika',
+    lastName: 'Karki',
+    email: 'monika@gmail.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Deepika',
+    lastName: 'Singh',
+    email: 'deepika@gmail.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Garima',
+    lastName: 'Darmwal',
+    email: 'garima@gmail.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'Neema',
+    lastName: 'Barti',
+    email: 'neema@gmail.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Deepika',
+    lastName: 'Mishra',
+    email: 'deepika@gmail.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Nitika',
+    lastName: 'Tiwari',
+    email: 'nitika@gmail.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
